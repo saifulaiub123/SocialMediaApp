04619abd1b369430d46c46b6a08cc92b4f9b81e0
MD Saiful Islam <saifulprogrammer@gmail.com>
2024-11-04 00:03:05 +0600

    hide

--- Api/UM.Api/Controllers/RoleController.cs
+++ Api/UM.Api/Controllers/RoleController.cs
@@ -1,30 +1,30 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using UM.Application.Enum;
-using UM.Application.Response;
-using UM.Domain.DBModel;
-using UM.Domain.Model;
+﻿//using Microsoft.AspNetCore.Authorization;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Identity;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.EntityFrameworkCore;
+//using UM.Application.Enum;
+//using UM.Application.Response;
+//using UM.Domain.DBModel;
+//using UM.Domain.Model;
 
-namespace UM.Api.Controllers
-{
-    [Authorize]
-    public class RoleController : BaseController
-    {
-        private readonly RoleManager<Domain.DBModel.Role> _roleManager;
-        public RoleController(RoleManager<Domain.DBModel.Role> roleManager) 
-        { 
-            _roleManager= roleManager;
-        }
+//namespace UM.Api.Controllers
+//{
+//    [Authorize]
+//    public class RoleController : BaseController
+//    {
+//        private readonly RoleManager<Domain.DBModel.Role> _roleManager;
+//        public RoleController(RoleManager<Domain.DBModel.Role> roleManager) 
+//        { 
+//            _roleManager= roleManager;
+//        }
 
-        [HttpGet]
-        [Route("GetRoles")]
-        public async Task<IActionResult> GetRoles()
-        {
-            var roles = await _roleManager.Roles.ToListAsync();
-            return Ok(roles);
-        }
-    }
-}
+//        [HttpGet]
+//        [Route("GetRoles")]
+//        public async Task<IActionResult> GetRoles()
+//        {
+//            var roles = await _roleManager.Roles.ToListAsync();
+//            return Ok(roles);
+//        }
+//    }
+//}
